2e8db79671771c2722782a23a9ca24313f181a2e
lucatam05 <tamiazzoluca2912@gmail.com>
2026-07-05 17:13:36 +0200

    testing kafka

--- Music.User.WebApi/Program.cs
+++ Music.User.WebApi/Program.cs
@@ -99,7 +99,8 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-
+Console.WriteLine($"Kafka BootstrapServers: {builder.Configuration["Kafka:ConsumerClient:BootstrapServers"]}");
+Console.WriteLine($"Kafka GroupId: {builder.Configuration["Kafka:ConsumerClient:GroupId"]}");
 var app = builder.Build();
 
 // Migrations
