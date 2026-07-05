32a4e4fb6cf8e4b8ed566097a4a4f1df5697acaa
lucatam05 <tamiazzoluca2912@gmail.com>
2026-07-05 20:42:52 +0200

    update kafka topics

--- Music.User.WebApi/Kafka/UserKafkaTopics.cs
+++ Music.User.WebApi/Kafka/UserKafkaTopics.cs
@@ -3,9 +3,11 @@ namespace MusicUser.Kafka;
 public class UserKafkaTopics : AbstractKafkaTopics
 {
     public string SongAdded { get; set; } = "song-added-to-library";
+    public string SongRemoved { get; set; } = "song-removed-from-library";
     
     public override IEnumerable<string> GetTopics()
     {
         yield return SongAdded;
+        yield return SongRemoved;
     }
 }
